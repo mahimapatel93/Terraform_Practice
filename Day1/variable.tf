24be88a1f82acf1d83785f79e9f40da114b76179
Mahima <patelmahi54295@gmail.com>
2025-12-30 15:36:46 +0530

    add

---
+++ Day1/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+    description = "The AMI ID for the EC2 instance"
+    type        = string
+    default     = ""   
+}
+variable "instance_type" {
+    description = "The type of instance to use"
+    type        = string
+    default     = ""
+}
