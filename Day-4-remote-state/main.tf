575d94172c5edbfc93e9e28b7d2f99d8b754a6c6
Mahima <patelmahi54295@gmail.com>
2026-01-03 18:18:55 +0530

    Day-4-remote-state/main.tf

--- Day-4-remote-state/main.tf
+++ Day-4-remote-state/main.tf
@@ -1,4 +1,20 @@
 resource "aws_instance" "name" {
     ami      = "ami-068c0051b15cdb816"
     instance_type = "t2.micro"
+       tags = {
+        Name = "remote-state-instance"
+    }
+}
+
+resource "aws_instance" "dev" {
+    ami      = "ami-068c0051b15cdb816"
+    instance_type = "t2.micro"
+       tags = {
+        Name = "ec2-instance"
+    }
+}
+
+
+resource "aws_s3_bucket" "name" {
+    bucket = "my-remote-bucket111111111111111"
 }
