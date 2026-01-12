da8cd1ca283e77c28d933049e062258229a5b4c7
Mahima <patelmahi54295@gmail.com>
2026-01-13 01:07:32 +0530

    add day-10-Terget-resource

---
+++ day-10-Terget-resource/main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "name" {
+    ami      = "ami-068c0051b15cdb816"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+       tags = {
+        Name = "instance"
+    }
+}
+
+
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "cust-vpc"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#cammand :------------------
+#terraform plan -target=aws_instance.name
+# terraform apply -target=aws_instance.name
