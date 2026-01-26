1f9dd93ae41c5aa521e321854ae2f1b9380d3912
Mahima Patel <159768314+mahimapatel93@users.noreply.github.com>
2026-01-26 12:00:50 +0530

    Update variable.tf

--- Day-1/variable.tf
+++ Day-1/variable.tf
@@ -1,10 +1,10 @@
 variable "ami_id" {
     description = "The AMI ID for the EC2 instance"
     type        = string
-    default     = ""   
+    default     = "ami-068c0051b15cdb816"   
 }
 variable "instance_type" {
     description = "The type of instance to use"
     type        = string
-    default     = ""
+    default     = "t2.micro"
 }
